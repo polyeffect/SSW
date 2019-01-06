61fb1fd51ecad1cd1758479c73546ac8b015fde8
polyeffect <polyeffect@gmail.com>
2019-01-06 09:20:27 +0900

    Add files via upload

--- Arduino/IMUUDP/IMUUDP.ino
+++ Arduino/IMUUDP/IMUUDP.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include "MPU9250.h"
 
 // Wi-Fi connection variables
 const char* ssid = "ssw";
@@ -15,18 +16,40 @@ WiFiUDP UDP;
 boolean udpConnected = false;
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
 
+// an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
+MPU9250 IMU(Wire, 0x68);
+int status;
+float sensorVal = 0;
+float EMA_A = 0.6;
+float EMA_S = 0;
+float isMoving = false;
+
 // variables
 char ReplyBuffer[] = "IMU UDP acknowledge";
 
 void setup() {
   Serial.begin(115200);
+  while(!Serial) {}
+  
   wifiConnected = connectWiFi();
 
   if (wifiConnected) {
     udpConnected = connectUDP();
     if (udpConnected) {
+      // IMU
+      status = IMU.begin();
+      if (status < 0) {
+        Serial.println("IMU initialization unsuccessful");
+        Serial.println("Check IMU wiring or try cycling power");
+        Serial.print("Status: ");
+        Serial.println(status);
+        while(1) {}
+      }
+      
       // PIN Setup
-
+      pinMode(BUILTIN_LED, OUTPUT);
+      pinMode(D3, OUTPUT);
+      pinMode(D7, OUTPUT);
     }
   }
 }
@@ -60,12 +83,50 @@ void loop() {
         UDP.endPacket();
       }
 
+      // Read the IMU sensor
+      IMU.readSensor();
+      sensorVal = IMU.getGyroZ_rads();
+      EMA_S = (EMA_A * sensorVal) + ((1 - EMA_A) * EMA_S);
+
+      Serial.print(sensorVal);
+      Serial.print("\t");
+      Serial.println(EMA_S);
+      
+      if (EMA_S < -0.4 && !isMoving) {
+        isMoving = true;
+        digitalWrite(D7, 1);
+        digitalWrite(BUILTIN_LED, 1);
+        digitalWrite(D3, 0);
+        char sendBuffer[] = "imuReverse";
+        UDP.beginPacket(UDPBroadcast, UDPPort);
+        UDP.write(sendBuffer);
+        UDP.endPacket();
+      } else if (EMA_S >= -0.04 && EMA_S <= 0.4 && isMoving) {
+        isMoving = false;
+        digitalWrite(D7, 0);
+        digitalWrite(BUILTIN_LED, 0);
+        digitalWrite(D3, 0);
+        char sendBuffer[] = "imuIdle";
+        UDP.beginPacket(UDPBroadcast, UDPPort);
+        UDP.write(sendBuffer);
+        UDP.endPacket();
+      } else if (EMA_S > 0.4 && !isMoving) {
+        isMoving = true;
+        digitalWrite(D7, 0);
+        digitalWrite(BUILTIN_LED, 1);
+        digitalWrite(D3, 1);
+        char sendBuffer[] = "imuObverse";
+        UDP.beginPacket(UDPBroadcast, UDPPort);
+        UDP.write(sendBuffer);
+        UDP.endPacket();
+      }
+
       // interaction
       
     }
   }
 
-  delay(10);
+  delay(100);
 }
 
 
